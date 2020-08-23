4111e1d744b91b8bc4f05732543b10926f870922
jeng832 <jeng832@gmail.com>
2020-08-23 13:07:18 +0900

    15 3Sum

--- 15_3Sum/src/main/java/Solution.java
+++ 15_3Sum/src/main/java/Solution.java
@@ -3,42 +3,42 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 class Solution {
-    private Map<Integer, List<List<Integer>>> subSumMap;
-
-    private Map<Integer, List<List<Integer>>> makeSubSumMap(int[] nums) {
-        Map<Integer, List<List<Integer>>> subSumMap = new HashMap<>();
+    private Map<Integer, Set<List<Integer>>> makeSubSumMap(int[] nums) {
+        Map<Integer, Set<List<Integer>>> subSumMap = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
-                if (subSumMap.containsKey(nums[i] + nums[j])) {
-                    List<Integer> list = new ArrayList<>();
-                    list.addAll(Arrays.asList(nums[i], nums[j]));
-                    subSumMap.get(nums[i] + nums[j]).add(list);
+                Integer key = nums[i] + nums[j];
+                List<Integer> indexList = new ArrayList<>();
+                indexList.add(i);
+                indexList.add(j);
+                indexList.sort(Comparator.comparingInt(e -> nums[e]));
+                if (subSumMap.containsKey(key)) {
+                    subSumMap.get(key).add(indexList);
                 } else {
-                    List<List<Integer>> subSum = new ArrayList<>();
-                    List<Integer> list = new ArrayList<>();
-                    list.addAll(Arrays.asList(nums[i], nums[j]));
-                    subSum.add(list);
-                    subSumMap.put(nums[i] + nums[j], subSum);
+                    Set<List<Integer>> set = new HashSet<>();
+                    set.add(indexList);
+                    subSumMap.put(key, set);
                 }
-
             }
         }
         return subSumMap;
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
-        subSumMap = makeSubSumMap(nums);
+        Map<Integer, Set<List<Integer>>> subSumMap = makeSubSumMap(nums);
         List<List<Integer>> ret = new ArrayList<>();
-        Set<Integer> set = IntStream.of(nums).boxed()
-                .collect(Collectors.toCollection(HashSet::new));
-        for (int num : set) {
-            List<List<Integer>> subSum = subSumMap.get(num * -1);
-            if (subSum == null) continue;
-            subSum.stream().forEach(list -> {
-                list.add(num);
-                list.sort(Integer::compareTo);
-            });
-            ret.addAll(subSum);
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (subSumMap.containsKey(num * -1)) {
+                Set<List<Integer>> subSum = subSumMap.get(num * -1);
+                for (List<Integer> elem : subSum) {
+                    if (elem.contains(i)) continue;
+                    List<Integer> list = new ArrayList<>(elem).stream().map(e -> nums[e]).collect(Collectors.toList());
+                    list.add(num);
+                    list.sort(Integer::compareTo);
+                    ret.add(list);
+                }
+            }
         }
         return ret.stream().distinct().collect(Collectors.toList());
     }
