58c85ac709245aa1e83b2ec217a75f559af48081
jeng832 <jeng832@gmail.com>
2020-08-23 12:59:31 +0900

    21 Merge Two Sorted Lists

--- 21_Merge_Two_Sorted_Lists/src/main/java/Solution.java
+++ 21_Merge_Two_Sorted_Lists/src/main/java/Solution.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Solution {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null && l2 == null) return null;
+        ListNode node = new ListNode();
+        ListNode current = node;
+        ListNode currentL1 = l1;
+        ListNode currentL2 = l2;
+        while (currentL1 != null && currentL2 != null) {
+            if (currentL1.val < currentL2.val) {
+                current.val = currentL1.val;
+                currentL1 = currentL1.next;
+            } else {
+                current.val = currentL2.val;
+                currentL2 = currentL2.next;
+            }
+            current.next = new ListNode();
+            current = current.next;
+        }
+        if (currentL1 != null) {
+            while (currentL1 != null) {
+                current.val = currentL1.val;
+                currentL1 = currentL1.next;
+                if (currentL1 != null) {
+                    current.next = new ListNode();
+                    current = current.next;
+                }
+            }
+        }
+        if (currentL2 != null) {
+            while (currentL2 != null) {
+                current.val = currentL2.val;
+                currentL2 = currentL2.next;
+                if (currentL2 != null) {
+                    current.next = new ListNode();
+                    current = current.next;
+                }
+            }
+        }
+        return node;
+    }
 }
